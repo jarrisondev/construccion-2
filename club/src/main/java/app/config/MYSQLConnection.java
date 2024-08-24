82b9dccc68961b9d04758364c3adb0eeb5fa818a
Jarrison Cano <me@jarrison.dev>
2024-08-24 09:07:42 -0500

    remove root password

--- club/src/main/java/app/config/MYSQLConnection.java
+++ club/src/main/java/app/config/MYSQLConnection.java
@@ -10,7 +10,7 @@ public class MYSQLConnection {
 
 	private static final String URL = "jdbc:mysql://localhost:3306/club_db";
 	private static final String USER = "root";
-	private static final String PASSWORD = "root";
+	private static final String PASSWORD = "";
 
 	public static Connection getConnection() {
 		Connection connection = null;
